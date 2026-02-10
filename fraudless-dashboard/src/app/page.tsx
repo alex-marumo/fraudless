092a36cef57649599fc519383946f0858af4866e
alex-marumo <alexmarumo16@gmail.com>
2026-02-10 19:40:47 +0200

    feat: dashboard for the fraud detector

--- fraudless-dashboard/src/app/page.tsx
+++ fraudless-dashboard/src/app/page.tsx
@@ -1,64 +1,121 @@
-import Image from "next/image";
+"use client";
+import React, { useState } from "react";
+import { ShieldAlert, ShieldCheck, Activity } from "lucide-react";
 
-export default function Home() {
+export default function FraudlessDashboard() {
+  const [status, setStatus] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+
+  const checkFraud = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setLoading(true);
+    const formData = new FormData(e.currentTarget);
+
+    const data = {
+      amount: parseFloat(formData.get("amount") as string),
+      time_of_day: parseFloat(formData.get("time") as string),
+      distance: parseFloat(formData.get("distance") as string),
+    };
+
+    try {
+      const results = await fetch("http://127.0.0.1:8000/check-transaction", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data),
+      });
+      const result = await res.json();
+      setStatus(result);
+    } catch (err) {
+      console.error("Server offline:", err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
+    <div className="max-w-4xl mx-auto grid md:grid-cols-2 gap-12,">
+      <header className="mb-12 flex items-center gap-3">
+        <Activity className="text-emerald-500" />
+        <h1 className="text-2xl font-bold tracking-tight">Fraudlesss</h1>
+      </header>
+
+      <main className="max-w-4xl mx-auto grid md:grid-cols-2 gap-12">
+        {/*Input Form*/}
+        <section className="bg-slate-900 p-6 rounded-xl border border-slate-800">
+          <h2 className="text-lg mb-4 fnt-semibold text-slate-400">
+            Test Transactions
+          </h2>
+          <form onSubmit={checkFraud} className="space-y-4">
+            <div>
+              <label className="block text-xs uppercase mb-1">
+                Amount (BWP)
+              </label>
+              <input
+                name="amount"
+                type="number"
+                step="0.01"
+                className="w-full bg-slate-800 border-none rounded p-2"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-xs uppercase mb-1">
+                Time (0-24)
+              </label>
+              <input
+                name="time"
+                tyoe="number"
+                step="0.1"
+                className="w-full bg-slate-800 border-slate-800 rounded p-2"
+                required
+              />
+            </div>
+            <div>
+              <label className="block text-xs uppercase mb-1">
+                Distance From Home (km)
+              </label>
+              <input
+                name="distance"
+                type="number"
+                step="0.1"
+                className="w-full bg-slate-800 border-none rounded p-2"
+                required
+              />
+            </div>
+            <button className="w-full bg-emerald-600 hover:bg-emerald-500 py-2 rounded font-bold transition">
+              {loading ? "Analyzing ..." : "Check Safety"}
+            </button>
+          </form>
+        </section>
+
+        <section className="flex flex-col items-center justify-center bg-slate-900 rounded-xl border border-slate-800">
+          {!status ? (
+            <p className="text-slate-500">Run a check to see results</p>
+          ) : (
+            <div className="text-center p-8">
+              {status.fraud_detected ? (
+                <>
+                  <ShieldAlert
+                    size={64}
+                    className="text-rose-500 mx-auto mb-4 animate-pulse"
+                  />
+                  <h3 className="text-2xl font-bold text-rose-500">
+                    FRAUD DETECTED
+                  </h3>
+                </>
+              ) : (
+                <>
+                  <ShieldCheck
+                    size={64}
+                    className="text-emerald-500 mx-auto mb-4"
+                  />
+                  <h3 className="text-2xl font-bold text-emerald-500">CLEAR</h3>
+                </>
+              )}
+              <p className="mt-2 text-slate-400 ">Action: {status.action}</p>
+            </div>
+          )}
+        </section>
       </main>
     </div>
   );
